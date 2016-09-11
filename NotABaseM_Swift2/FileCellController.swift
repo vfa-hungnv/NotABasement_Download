0558787c91162e75641151a5864c5f572a0c9999
vfa-hungnv <donmichael1Root@donmichaels-MacBook-Pro.local>
2016-09-11 12:44:21 +0700

    Load json OK

---
+++ NotABaseM_Swift2/FileCellController.swift
@@ -0,0 +1,24 @@
+//
+//  FileCellController.swift
+//  NotABaseM_Swift2
+//
+//  Created by Hung Nguyen on 9/11/16.
+//  Copyright © 2016 Hung Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class FileCellController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
